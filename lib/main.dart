b3377e1b8bdff7f1f5a7f5a88340f16e387e32b2
KeyndraPrawira <keyndraardhiprawira@gmail.com>
2025-07-28 16:49:11 +0700

    28 juli 2025

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:belajar_flutter/main_layout.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HelloFlutter()
+    );
+  }
+}
+
+class HelloFlutter extends StatelessWidget {
+  const HelloFlutter({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: 'Belajar v Flutter',
+      body: Center(
+        child:  Text('Hello Flutter!',
+                style: TextStyle(fontSize: 24, color: const Color.fromARGB(255, 255, 255, 255), 
+                fontWeight: FontWeight.bold, 
+                backgroundColor: Colors.blueAccent),
+                ),
+      ),
+    );
+  }
+}
